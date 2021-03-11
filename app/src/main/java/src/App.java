222d846a204da00176ed266ea262df64455b9dcf
Juan Pablo Gonzales <juampi7237@gmail.com>
2021-03-11 10:54:58 -0400

    clean App class and AppTest

--- app/src/main/java/src/App.java
+++ app/src/main/java/src/App.java
@@ -4,11 +4,7 @@
 package src;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    private static void main(final String[] args) {
+        System.out.println("hola mundo");
     }
 }
