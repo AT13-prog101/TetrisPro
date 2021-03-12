8ab40cfce55d10d8392cc310f42e5351e67eca47
JessickaMoya <jessicka.moya@gmail.com>
2021-03-12 09:09:31 -0400

    Separate unit tes in RandomShapeTest

--- app/src/test/java/src/RandomShapeTest.java
+++ app/src/test/java/src/RandomShapeTest.java
@@ -7,21 +7,51 @@ import static org.junit.Assert.*;
 public class RandomShapeTest {
 
     @Test
-    public void getShape_GetRandomNumber_RandomShape() {
+    public void getShape_GetRandomNumber_ShapeI() {
         RandomShape randomShape = new RandomShape();
         ShapeI shapeI = new ShapeI();
-        ShapeJ shapeJ = new ShapeJ();
-        ShapeL shapeL = new ShapeL();
-        ShapeO shapeO = new ShapeO();
-        ShapeS shapeS = new ShapeS();
-        ShapeT shapeT = new ShapeT();
-        ShapeZ shapeZ = new ShapeZ();
         assertEquals(shapeI.getClass(),randomShape.getShape(0).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeJ() {
+        RandomShape randomShape = new RandomShape();
+        ShapeJ shapeJ = new ShapeJ();
         assertEquals(shapeJ.getClass(),randomShape.getShape(1).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeL() {
+        RandomShape randomShape = new RandomShape();
+        ShapeL shapeL = new ShapeL();
         assertEquals(shapeL.getClass(),randomShape.getShape(2).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeO() {
+        RandomShape randomShape = new RandomShape();
+        ShapeO shapeO = new ShapeO();
         assertEquals(shapeO.getClass(),randomShape.getShape(3).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeS() {
+        RandomShape randomShape = new RandomShape();
+        ShapeS shapeS = new ShapeS();
         assertEquals(shapeS.getClass(),randomShape.getShape(4).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeT() {
+        RandomShape randomShape = new RandomShape();
+        ShapeT shapeT = new ShapeT();
         assertEquals(shapeT.getClass(),randomShape.getShape(5).getClass());
+    }
+
+    @Test
+    public void getShape_GetRandomNumber_ShapeZ() {
+        RandomShape randomShape = new RandomShape();
+        ShapeZ shapeZ = new ShapeZ();
         assertEquals(shapeZ.getClass(),randomShape.getShape(6).getClass());
     }
 }
