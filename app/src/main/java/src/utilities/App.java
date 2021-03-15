5764f9e7f01b53840c5acd9defad03a4d7f9dd4e
JessickaMoya <jessicka.moya@gmail.com>
2021-03-14 22:45:37 -0400

    Change movementMenu method from App to GameBoard

--- app/src/main/java/src/utilities/App.java
+++ app/src/main/java/src/utilities/App.java
@@ -3,48 +3,10 @@
  */
 package src.utilities;
 
-import java.util.Scanner;
-
 public class App {
-    private static final int OPTION_1 = 1;
-    private static final int OPTION_2 = 2;
-    private static final int OPTION_3 = 3;
-    private static final int OPTION_4 = 4;
 
     public static void main(final String[] args) {
-        movementMenu();
-    }
-
-    static void movementMenu() {
-        boolean gameInCourse = true;
-        Scanner scanner = new Scanner(System.in);
-        while (gameInCourse) {
-            System.out.println("Press next numbers to");
-            System.out.println("1.- Move to Right");
-            System.out.println("2.- Move to Left");
-            System.out.println("3.- Rotate to Right");
-            System.out.println("4.- Rotate to Left");
-            System.out.println("5.- Exit");
-            int option = scanner.nextInt();
-            switch (option) {
-                case OPTION_1:
-                    System.out.println("Moved to right");
-                    break;
-                case OPTION_2:
-                    System.out.println("Moved to left");
-                    break;
-                case OPTION_3:
-                    System.out.println("Rotated to right");
-                    break;
-                case OPTION_4:
-                    System.out.println("Rotated to left");
-                    break;
-                default:
-                    gameInCourse = false;
-                    break;
-            }
-        }
-        scanner.close();
+        System.out.println("hola mundo");
     }
 }
 
--- app/src/main/java/src/utilities/GameBoard.java
+++ app/src/main/java/src/utilities/GameBoard.java
@@ -1,10 +1,16 @@
 package src.utilities;
 
+import java.util.Scanner;
+
 public class GameBoard {
     private Player player;
     private int speed;
     static final int GAME_BOARD_HEIGHT = 20;
     static final int GAME_BOARD_WIDTH = 10;
+    private static final int OPTION_1 = 1;
+    private static final int OPTION_2 = 2;
+    private static final int OPTION_3 = 3;
+    private static final int OPTION_4 = 4;
 
     public void init() { }
 
@@ -51,5 +57,40 @@ public class GameBoard {
             }
         }
         return (notEmptyValueCounter == gameBoardArray[row].length);
+    }
+
+    /**
+     * Prints the options for movement menu
+     */
+    public void movementMenu() {
+        boolean gameInCourse = true;
+        Scanner scanner = new Scanner(System.in);
+        while (gameInCourse) {
+            System.out.println("Press next numbers to");
+            System.out.println("1.- Move to Right");
+            System.out.println("2.- Move to Left");
+            System.out.println("3.- Rotate to Right");
+            System.out.println("4.- Rotate to Left");
+            System.out.println("5.- Exit");
+            int option = scanner.nextInt();
+            switch (option) {
+                case OPTION_1:
+                    System.out.println("Moved to right");
+                    break;
+                case OPTION_2:
+                    System.out.println("Moved to left");
+                    break;
+                case OPTION_3:
+                    System.out.println("Rotated to right");
+                    break;
+                case OPTION_4:
+                    System.out.println("Rotated to left");
+                    break;
+                default:
+                    gameInCourse = false;
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
