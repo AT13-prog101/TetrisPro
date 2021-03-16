a332500b30b6fb2439b52b7c805f1172e535a615
Raymundo Guaraguara <raymundogs@gmail.com>
2021-03-15 23:31:49 -0400

    Create method adjustlines with its unit test

--- app/src/main/java/src/utilities/GameBoard.java
+++ app/src/main/java/src/utilities/GameBoard.java
@@ -5,6 +5,33 @@ public class GameBoard {
     private int speed;
     static final int GAME_BOARD_HEIGHT = 20;
     static final int GAME_BOARD_WIDTH = 10;
+    private boolean[][] gameBoardArray;
+
+    public GameBoard () {
+        this.gameBoardArray = createGameBoardArray(GAME_BOARD_HEIGHT, GAME_BOARD_WIDTH);
+    }
+
+    /**
+     * Sets the values of the game board array according to a position and an array
+     * @param x initial position on the x axis
+     * @param y initial position on the y axis
+     * @param array the array that contains the values to change
+     */
+    public void setGameBoardArray(int x, int y, boolean[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                this.gameBoardArray[i + y][j + x] = array[i][j];
+            }
+        }
+    }
+
+    /**
+     * Obtains the game board array
+     * @return the game board array
+     */
+    public boolean[][] getGameBoardArray() {
+        return this.gameBoardArray;
+    }
 
     public void init() { }
 
@@ -38,18 +65,43 @@ public class GameBoard {
     }
 
     /**
-     * Verifies if a certain row is full of true
+     * Verifies if a certain row is full of a certain value
      * @param gameBoardArray is the array to check
      * @param row is the row to be verified
+     * @param arrayValue is the value to check
      * @return a boolean according to the verification made
      */
-    public boolean checkFullRow(final boolean[][] gameBoardArray, final int row) {
-        int notEmptyValueCounter = 0;
+    public boolean checkFullRow(final boolean[][] gameBoardArray, final int row, final boolean arrayValue) {
+        int valueCounter = 0;
         for (int j = 0; j < gameBoardArray[row].length; j++) {
-            if (gameBoardArray[row][j] == true) {
-                notEmptyValueCounter += 1;
+            if (gameBoardArray[row][j] == arrayValue) {
+                valueCounter += 1;
             }
         }
-        return (notEmptyValueCounter == gameBoardArray[row].length);
+        return (valueCounter == gameBoardArray[row].length);
+    }
+
+    public void adjustLines(final boolean[][] gameBoardArray) {
+        int row = gameBoardArray.length - 1;
+        int emptyRowCounter;
+        while (row >= 0) {
+            emptyRowCounter = 0;
+            while (checkFullRow(gameBoardArray, row, false)) {
+                emptyRowCounter += 1;
+                row -= 1;
+            }
+            if (emptyRowCounter == 0) {
+                row -= 1;
+            } else {
+                boolean[][] array = new boolean[1][gameBoardArray[row].length];
+                for (int j = 0; j < gameBoardArray[row].length; j++) {
+                    array[0][j] = gameBoardArray[row][j];
+                }
+                setGameBoardArray(0, row + emptyRowCounter, array);
+                cleanRowOnArray(gameBoardArray, row);
+                setGameBoardArray(0, 0, gameBoardArray);
+                row = row + emptyRowCounter - 1;
+            }
+        }
     }
 }
--- app/src/test/java/src/GameBoardTest.java
+++ app/src/test/java/src/GameBoardTest.java
@@ -27,7 +27,7 @@ public class GameBoardTest {
     public void checkFullRow_ArrayOf2x5andRow0_True() {
         GameBoard gameBoard = new GameBoard();
         boolean[][] array = {{true, true, true, true, true}, {false, true, false, true, false}};
-        boolean actual = gameBoard.checkFullRow(array,0);
+        boolean actual = gameBoard.checkFullRow(array,0, true);
         assertTrue(actual);
     }
 
@@ -35,7 +35,91 @@ public class GameBoardTest {
     public void checkFullRow_ArrayOf2x5andRow1_False() {
         GameBoard gameBoard = new GameBoard();
         boolean[][] array = {{true, true, true, true, true}, {false, true, false, true, false}};
-        boolean actual = gameBoard.checkFullRow(array,1 );
+        boolean actual = gameBoard.checkFullRow(array,1, true);
         assertFalse(actual);
+    }
+
+    @Test
+    public void setGameBoardArray_1And2AndArrayOf2x2_UpdatedArray() {
+        GameBoard gameBoard = new GameBoard();
+        boolean[][] arrayToChange = {{true, true}, {true, true}};
+        gameBoard.setGameBoardArray(1, 2, arrayToChange);
+        boolean[][] actual = gameBoard.getGameBoardArray();
+        boolean [][] expected = {
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, true, true, false, false, false, false, false, false, false},
+                {false, true, true, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false}
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void adjustLines_GameBoardArray_OrderedGameBoardArray() {
+        GameBoard gameBoard = new GameBoard();
+        boolean[][] disorderedArray = {
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {true, true, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {true, false, false, false, true, false, false, false, false, true},
+                {true, false, true, false, false, true, false, false, true, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {true, false, true, false, false, false, true, false, false, false}
+        };
+        gameBoard.setGameBoardArray(0, 0, disorderedArray);
+        gameBoard.adjustLines(gameBoard.getGameBoardArray());
+        boolean[][] actual = gameBoard.getGameBoardArray();
+        boolean [][] expected = {
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {true, false, false, false, false, false, false, false, false, false},
+                {true, true, false, false, false, false, false, false, false, false},
+                {true, false, false, false, true, false, false, false, false, true},
+                {true, false, true, false, false, true, false, false, true, false},
+                {true, false, true, false, false, false, true, false, false, false}
+        };
+        assertArrayEquals(expected, actual);
     }
 }
